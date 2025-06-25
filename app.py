db4d46f92eb12602b4473141831722d6e6f9eb63
dotun2203 <adedotunabimbola2@gmail.com>
2025-06-25 14:50:45 +0100

    added port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from scraper.scrape_nysc import scrape_nysc
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -24,3 +25,7 @@ def scrape():
 @app.route('/api/download/<filename>', methods=['GET'])
 def download_file(filename):
     return send_file(f"./outputs/{filename}", as_attachment=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
